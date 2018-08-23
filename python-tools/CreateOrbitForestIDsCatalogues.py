b35279533c1c725e4c234ea252157381d32955a6
rhyspoulton <rhyspoulton92@gmail.com>
2018-08-23 04:31:58 +0000

    Update to mass definition
    OrbWeaver now uses Mass_tot as a definition of mass, which is the total mass of the particles within Rvir as this is a much better tracer of mass than Mvir which was previously used.

--- python-tools/CreateOrbitForestIDsCatalogues.py
+++ python-tools/CreateOrbitForestIDsCatalogues.py
@@ -7,7 +7,7 @@ from scipy.interpolate import interp1d
 
 np.set_printoptions(threshold=10)
 
-atime,tree,numhalos,halodata,cosmodata,unitdata = VPT.ReadUnifiedTreeandHaloCatalog("/mnt/su3ctm/rpoulton/waves/analysis/waves_40_512/VELOCIraptor.tree.t4.unifiedhalotree",desiredfields=["ID","Head","Tail","RootTail","RootHead","npart","Mass_200crit","R_200crit","Xc","Yc","Zc","VXc","VYc","VZc","Vmax","Rmax","cNFW"])
+atime,tree,numhalos,halodata,cosmodata,unitdata = VPT.ReadUnifiedTreeandHaloCatalog("/mnt/su3ctm/rpoulton/waves/analysis/waves_40_512/VELOCIraptor.tree.t4.unifiedhalotree",desiredfields=["ID","Head","Tail","RootTail","RootHead","npart","Mass_200crit","R_200crit","Xc","Yc","Zc","VXc","VYc","VZc","Vmax","Rmax","cNFW","Mass_tot","Mass_FOF"])
 
 atime = atime[::-1]
 numhalos = numhalos[::-1]
@@ -27,7 +27,7 @@ outfilebasename = "/mnt/su3ctm/rpoulton/orbitdata/cat"
 
 # Build a new data stucture to contain the information for this file
 treefields = ["origID","ID","Head","Tail","OrbitingHaloID","hostHaloID"]
-orbitalfields = ["Mass_200crit","R_200crit","npart","Xc","Yc","Zc","VXc","VYc","VZc","Vmax","Rmax","cNFW"]
+orbitalfields = ["Mass_200crit","Mass_tot","Mass_FOF","R_200crit","npart","Xc","Yc","Zc","VXc","VYc","VZc","Vmax","Rmax","cNFW"]
 orbitdata = [{field:[] for field in orbitalfields+treefields} for snap in range(numsnaps)]
 
 #initialize the dictionaries
