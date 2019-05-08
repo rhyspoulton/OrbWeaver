a221129da0e09dc07cbbbcde52adf2f85f0984d7
rhyspoulton <rhyspoulton92@gmail.com>
2019-05-08 02:39:21 +0000

    Updated the orbweaver_tools
    Enabled the orbweaver_tools to read only the desired fields and the file number for the entry if requested also added a docstring to the function.

--- python-tools/orbweaver_tools.py
+++ python-tools/orbweaver_tools.py
@@ -3,8 +3,31 @@ import h5py
 import os
 
 
-def ReadOrbitData(filenamelist):
+def ReadOrbitData(filenamelist,iFileno=False,desiredfields=[]):
 
+	"""
+
+	Function to read in the data from the .orbweaver.orbitdata.hdf files
+
+	Parameters
+	----------
+
+	filenamelist : str
+		The file containing the basenames for the orbweaver catalogue (can use the file generated from the creation of the (preprocessed) orbit catalogue)
+
+	iFileno : bool, optional
+		If True, the file number where the each of the entries came from is to be outputted.
+
+	desiredfields : list, optional
+		A list containing the desired fields to put returned, please see the FieldsDescriptions.md for the list of fields availible. If not supplied then all fields are returned
+
+	Returns
+	-------
+
+	orbitdata : dict
+		Dictionary of the fields to be outputted, where each field is a ndarray .
+
+	"""
 
 	#First see if the file exits
 	if(os.path.isfile(filenamelist)==False):
@@ -18,7 +41,7 @@ def ReadOrbitData(filenamelist):
 	except ValueError:
 		raise IOError("The first line of the filelist (which says the number of files), cannot be interpreted as a integer")
 
-	fileno = np.zeros(numfiles,dtype=np.int32)
+	if(iFileno): fileno = np.zeros(numfiles,dtype=np.int32)
 	numentries = np.zeros(numfiles,dtype=np.uint64)
 	maxorbitIDs = np.zeros(numfiles,dtype=np.uint64)
 	prevmaxorbitID = np.uint64(0)
@@ -40,14 +63,18 @@ def ReadOrbitData(filenamelist):
 		hdffile = h5py.File(filename,"r")
 
 		#Read the header information
-		fileno[i] = np.int32(hdffile.attrs["Fileno"][...])
+		if(iFileno): fileno[i] = np.int32(hdffile.attrs["Fileno"][...])
 		numentries[i] =  np.uint64(hdffile.attrs["Number_of_entries"][...])
 		maxorbitIDs[i] = prevmaxorbitID
 		prevmaxorbitID += np.uint64(hdffile.attrs["Max_orbitID"][...])
 
 		#If the first file then file then find the dataset names and their datatypes
 		if(i==0):
-			orbitdatakeys = list(hdffile.keys())
+			if(len(desiredfields)>0):
+				orbitdatakeys = desiredfields
+			else:
+				orbitdatakeys = list(hdffile.keys())
+
 			for key in orbitdatakeys:
 				orbitdatatypes[key] = hdffile[key].dtype
 
@@ -60,8 +87,9 @@ def ReadOrbitData(filenamelist):
 	totnumentries = np.sum(numentries)
 	orbitdata = {key:np.zeros(totnumentries,dtype = orbitdatatypes[key]) for key in orbitdatakeys}
 
-	#Add a field to contain the file number
-	orbitdata["Fileno"] = np.zeros(totnumentries,dtype=np.int32)
+	if(iFileno):
+		#Add a field to contain the file number
+		orbitdata["Fileno"] = np.zeros(totnumentries,dtype=np.int32)
 
 	ioffset = np.uint64(0)
 
@@ -83,11 +111,13 @@ def ReadOrbitData(filenamelist):
 		for key in orbitdatakeys:
 			orbitdata[key][startindex:endindex] = np.asarray(hdffile[key],dtype=orbitdatatypes[key])
 
-		#Lets offset the orbitID to make it unique across all the data
-		orbitdata["OrbitID"][startindex:endindex]+=maxorbitIDs[i]
+		if("OrbitID" in orbitdatakeys):
+			#Lets offset the orbitID to make it unique across all the data
+			orbitdata["OrbitID"][startindex:endindex]+=maxorbitIDs[i]
 
-		#Set the fileno that this came from
-		orbitdata["Fileno"][startindex:endindex]=fileno[i]
+		if(iFileno):
+			#Set the fileno that this came from
+			orbitdata["Fileno"][startindex:endindex]=fileno[i]
 
 		hdffile.close()
 
